e7820bf978522f57b4512907355942490717cd65
amailibri <rago.elise@gmail.com>
2024-11-20 14:37:51 -0500

    Update Final_project.py

--- Final_project.py
+++ Final_project.py
@@ -1,1 +1,9 @@
+import matplotlib as pyplot
+
+def load_data(filename):
+  with open(filename, "r") as infile:
+    homocide_data = []
+    line = infile.readline()
+    years = line.split(",")
+    
 
