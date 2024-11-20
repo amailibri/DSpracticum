186e81bc86e9ef78f9366a0e4ff1668847c7091f
amailibri <rago.elise@gmail.com>
2024-11-20 15:09:26 -0500

    Update Final_project.py

--- Final_project.py
+++ Final_project.py
@@ -1,9 +1,18 @@
 import matplotlib as pyplot
+import pandas as pd
 
-def load_data(filename):
-  with open(filename, "r") as infile:
-    homocide_data = []
-    line = infile.readline()
-    years = line.split(",")
-    
+
+def main():
+    illinois = pd.read_csv('Illinois_homicide_data_12-22.csv')
+    pivot_columns_illinois = illinois.transpose()
+    print(pivot_columns_illinois)
+    texas = pd.read_csv('Texas_homicide_data_12-22.csv')
+    pivot_columns_texas = texas.transpose()
+    print(pivot_columns_texas)
+
+    poverty = pd.read_csv('Poverty_data_12-22.csv')
+    # subset
+    in_poverty = poverty['Percent in Poverty']
+    print(in_poverty)
+main()
 
